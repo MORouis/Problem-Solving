fc92b12715475a7ac37cdfb89d14ffe19d58638b
MORouis <rouisousama@gmail.com>
2024-01-17 17:26:32 +0100

    correct condition

--- python/Lire_ou_ne_pas_lire,_telle_est_(à_nouveau)_la_question.py
+++ python/Lire_ou_ne_pas_lire,_telle_est_(à_nouveau)_la_question.py
@@ -5,7 +5,7 @@ def main():
     actual_book = liste[0]
     for i in range(len(liste)):
         liste[i] = input()
-        if liste[i]>=actual_book:
+        if liste[i]>actual_book:
             actual_book = liste[i]
             print(actual_book)
             
