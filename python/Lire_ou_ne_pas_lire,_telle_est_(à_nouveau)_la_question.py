c4b5b9956fda67d81c10fa5e02be8b1d6e901c69
MORouis <rouisousama@gmail.com>
2024-01-17 17:14:57 +0100

    Lire ou ne pas lire, telle est (à nouveau) la question(France-IOI)

---
+++ python/Lire_ou_ne_pas_lire,_telle_est_(à_nouveau)_la_question.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+
+    liste = [""]*n
+    actual_book = liste[0]
+    for i in range(len(liste)):
+        liste[i] = input()
+        if liste[i]>=actual_book:
+            actual_book = liste[i]
+            print(actual_book)
+            
+    
+main()
