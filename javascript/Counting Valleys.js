10cd1da066b0decc8d6a7a02ec014b35f6b0a363
MORouis <rouisousama@gmail.com>
2024-02-05 09:08:33 +0100

    Counting Valleys(Hacker Rank)

---
+++ javascript/Counting Valleys.js
@@ -0,0 +1,16 @@
+function countingValleys(steps, path) {
+  let valleys = 0;
+  let level = 0;
+  for (let i = 0; i < steps; i++) {
+    if (path[i] === "U") {
+      if (level === -1) {
+        valleys++;
+      }
+      level++;
+    } else {
+      level--;
+    }
+  }
+  return valleys;
+}
+
